059801c264d08f115156f512bc029bc1b7bf0d3f
JhonG <jgarcia@lsv-tech.com>
2022-01-16 08:56:33 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from "react";
+
+const todos = [
+  {text: 'Cortar cebolla', completed: false},
+  {text: 'Tomar el curso de React', completed: false},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+        {/*<TodoCounter />*/}
+        <h2> Has completado 2 de 3 TODOs</h2>
+
+        {/*<TodoSearch />*/}
+        <input placeholder="placeholder"/>
+
+{/*        <TodoList>
+          {todos.map(t =>  todos(
+              <TodoItem />
+          ))}
+        </TodoList>*/}
+
+        {/*<CreateTodoButton />*/}
+        <button>+</button>
+      </React.Fragment>
   );
 }
 
