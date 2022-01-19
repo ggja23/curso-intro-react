7ad80bff22cb2301aa0acd9b8ea399e1a2bfa2b2
Jhon Garcia <jgarcia@lsv-tech.com>
2022-01-19 07:28:05 -0500

    Add: Style to the components

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,18 @@ import React from "react";
 import "./TodoSearch.css"
 
 function TodoSearch(){
+
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+    };
+
+
     return(
-        <input className="TodoSearch" placeholder="placeholder"/>
+        <input
+            className="TodoSearch"
+            placeholder="placeholder"
+            onChange={onSearchValueChange}
+            />
     );
 }
 
