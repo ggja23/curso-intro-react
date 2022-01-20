f4b9f881dcebc6266de6ad84c9c6ab1137a4a1f0
Jhon Garcia <jgarcia@lsv-tech.com>
2022-01-19 23:13:21 -0500

    11_Organización de archivos y carpetas

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,5 @@
-// import './App.css';
 import React from "react";
-import {TodoCounter} from "./TodoCounter";
-import {TodoSearch} from "./TodoSearch";
-import {CreateTodoButton} from "./CreateTodoButton";
-import {TodoList} from "./TodoList";
-import {TodoItem} from "./TodoItem";
+import {AppUI} from "./AppUI";
 
 
 const defaultTodos = [
@@ -53,31 +48,15 @@ function App() {
     };
 
   return (
-      <React.Fragment>
-        <TodoCounter
-            total={totalTodos}
-            completed={completedTodos}
+      <AppUI
+          totalTodos={totalTodos}
+          completedTodos={completedTodos}
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+          searchedTodos={searchedTodos}
+          completeTodo={completeTodo}
+          deleteTodo={deleteTodo}
         />
-
-        <TodoSearch
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
-        />
-
-        <TodoList>
-          {searchedTodos.map( todo => (
-              <TodoItem
-                  key={todo.text}
-                  text={todo.text}
-                  completed={todo.completed}
-                  onComplete={() =>completeTodo(todo.text)}
-                  onDelete = {() => deleteTodo(todo.text)}
-              />
-          ))}
-        </TodoList>
-
-        <CreateTodoButton />
-      </React.Fragment>
   );
 }
 
